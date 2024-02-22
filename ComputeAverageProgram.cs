26d4eb01fa36c7e44ed7c0d91fe332c25473f3d9
DaphneMnlnsn <daphnemanalansan1213@gmail.com>
2024-02-23 07:22:30 +0800

    Add project files.

---
+++ ComputeAverageProgram.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ComputeAverageApp
+{
+    class ComputeAverageProgram
+    {
+        static void Main(string[] args)
+        {
+            double grade1, grade2, grade3, grade4, grade5, average;
+            int round;
+
+            Console.WriteLine("Enter 5 grades separated by new line: ");
+            grade1 = Convert.ToDouble(Console.ReadLine());
+            grade2 = Convert.ToDouble(Console.ReadLine());
+            grade3 = Convert.ToDouble(Console.ReadLine());
+            grade4 = Convert.ToDouble(Console.ReadLine());
+            grade5 = Convert.ToDouble(Console.ReadLine());
+
+            average = Convert.ToDouble((grade1 + grade2 + grade3 + grade4 + grade5) / 5);
+            round = Convert.ToInt32(Math.Round(average));
+
+            Console.WriteLine("The average is " + average + " and round off to " + round);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
